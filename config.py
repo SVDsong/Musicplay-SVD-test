10664f2475e1c7fa1264ddd6f3c0e6ae4ac5c334
SVDsong <140339928+SVDsong@users.noreply.github.com>
2023-07-28 13:29:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,17 +25,17 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
-        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
+        self.API_ID: str = os.environ.get("API_ID", "27957041")
+        self.API_HASH: str = os.environ.get("API_HASH", "2ae1c9912cd2efdecae7f0208994f0b0")
+        self.SESSION: str = os.environ.get("SESSION", "BQCO5p9kWJ8G5ju4XLWE_eMQQEEBXYcNKaOiJBVR6zL1KjAI1qZM7Pj01_qjldNCJWMvSRStqibhVC_wcTGviELKpfsYCvp8BE4rMOKF8l40ACvLzXSCLko_fkvJsr2_mr0mD_2M0uweFeEhNZGAxHooIOgb-cYmRxwDtoB34M92xDZGOS8mZJSXoKZolhhbVWgXXcQeUlliLYgg8B7GLYueO0-ynT32P_pY47I3V3fIH-6oTzHqQr9PGZ3XQRA1Q7mpHh4-ZPxLAofCFU6mB8CH-dYrYRX7obMfdjiklFEJUiJKmPwPfbUPYbe3CwqkS4TNvZqsWjp5OY1f9NMWiWyQAAAAATmDeAIA")
+        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "6041305578:AAHifZOeb5UURh-X8APKhBVWCidv8aZvI_A")
         self.SUDOERS: list = [
-            int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
+            int(id) for id in os.environ.get("SUDOERS", "5276467211").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("ERROR: SESSION, API_ID and API_HASH is required!")
             quit(0)
-        self.SPOTIFY: bool = False
+        self.SPOTIFY: bool = True
         self.QUALITY: str = os.environ.get("QUALITY", "high").lower()
         self.PREFIXES: list = os.environ.get("PREFIX", "!").split()
         self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
